7b9dfcbf8f9796b044d6fac0d79edac0dc13ba10
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-07 01:36:41 +0500

    Mantiqiy amallarga oid masalalar
    Mantiqiy amallarga oid masalalar

---
+++ 15_masala.py
@@ -0,0 +1,4 @@
+A=int(input("Birinchi butun sonni kiriting A => "))
+B=int(input("Ikkinchi butun sonni kiriting B => "))
+C=int(input("Ikkinchi butun sonni kiriting C => "))
+print((A*B*C<0) and ((A>0 and B>0 and C<0) or (A>0 and B<0 and C>0) or (A<0 and B>0 and C>0)))
