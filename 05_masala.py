7359139c487c2c0a9f44fd762844010ec1c75e28
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-07 01:27:33 +0500

    Mantiqiy amallarga oid masalalar
    Mantiqiy amallarga oid masalalar

---
+++ 05_masala.py
@@ -0,0 +1,3 @@
+A=int(input("Birinchi butun sonni kiriting A => "))
+B=int(input("Ikkinchi butun sonni kiriting B => "))
+print(A>=0 or B<-2)
---
+++ 08_masala.py
@@ -0,0 +1,3 @@
+A=int(input("Birinchi butun sonni kiriting A => "))
+B=int(input("Ikkinchi butun sonni kiriting B => "))
+print(A%2!=0 and B%2!=0)
